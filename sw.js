caf229a64e6ff6e22cb5f5054565e12a664bb598
Sonyschuk <sara@sostudio.ca>
2026-01-02 15:25:29 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,18 @@
-const CACHE = "intentional-bingo-v1";
+const CACHE = "intentional-bingo-v2";
 const ASSETS = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./sw.js",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
@@ -17,6 +21,7 @@ self.addEventListener("activate", (e) => {
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
     )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
